92447704005e58d2308cff6c3820fc765e85313b
e0 <shan.huang@me.com>
2021-08-17 11:21:53 +0200

    fix: add params validation

--- api/src/endpoints/dataOverlay.js
+++ api/src/endpoints/dataOverlay.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
+import models from 'data/integratedModels.json';
+
+const VALID_MODELS = models.map((m) => m.short_name);
 
 const routes = express.Router();
 
 routes.get('/:model', async (req, res) => {
   const { model } = req.params;
   try {
-    const indexJson = readFileSync(
+    if (!VALID_MODELS.includes(model)) {
+      throw new Error(`Invalid model provided: ${model}.`);
+    }
+
+    const indexJson = await readFile(
       `./dataOverlay/${model}/index.json`,
       'utf8',
     );
@@ -20,7 +27,21 @@ routes.get('/:model', async (req, res) => {
 routes.get('/:model/:dataType/:filename', async (req, res) => {
   const { model, dataType, filename } = req.params;
   try {
-    const dataSourceFile = readFileSync(
+    if (!VALID_MODELS.includes(model)) {
+      throw new Error(`Invalid model provided: ${model}.`);
+    }
+
+    const folderRegex = /^([a-zA-Z0-9][^*/><?\"|:]*)$/;
+    if (!dataType.match(folderRegex)) {
+      throw new Error(`Invalid data type provided: ${dataType}.`);
+    }
+
+    const fileRegex = /^[\w\-. ]+\.tsv$/i;
+    if (!filename.match(fileRegex)) {
+      throw new Error(`Invalid filename provided: ${filename}.`);
+    }
+
+    const dataSourceFile = await readFile(
       `./dataOverlay/${model}/${dataType}/${filename}`,
       'utf8',
     );
