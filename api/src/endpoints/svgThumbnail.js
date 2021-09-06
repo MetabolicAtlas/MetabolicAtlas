a7b04449c78a736a9368ec77fba58a93f1ae2778
nanjiangshu <nanjiang.shu@gmail.com>
2021-09-06 16:17:50 +0200

    feat: output SVG thumbnail file from API

--- api/src/endpoints/svgThumbnail.js
+++ api/src/endpoints/svgThumbnail.js
@@ -2,6 +2,7 @@ import express from 'express';
 import fs from 'fs';
 
 const routes = express.Router();
+const sharp = require('sharp');
 let HPA_JSON = null;
 
 routes.get('/:svgName', async (req, res) => {
@@ -10,9 +11,19 @@ routes.get('/:svgName', async (req, res) => {
 
   try {
     const svgFile = `/project/svg/${model}/${svgName}.svg`;
-    const svg = fs.readFileSync(svgFile, { encoding: 'utf8', flag: 'r' });
-    const svgThumbnail = svg;
-    res.json(svgThumbnail);
+    var svgThumbnail
+    await sharp(svgFile)
+      .resize(400)
+      .flatten({ background: { r: 255, g: 255, b: 255 } })
+      .png()
+      .toBuffer()
+      .then(function(info) {
+        svgThumbnail = info
+      })
+      .catch(function(err) {
+        console.log(err)
+      })
+    res.send(svgThumbnail);
   } catch (e) {
     if (e.message === '404') {
       return res.sendStatus(404);
