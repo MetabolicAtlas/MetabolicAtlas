9364789fb9add3aa19a15cbadc028d96a3c3559e
MalinAhlberg <malin.ahlberg@nbis.se>
2021-11-15 11:56:46 +0100

    feat: formatting for metabolites

--- frontend/src/helpers/chemical-formatters.js
+++ frontend/src/helpers/chemical-formatters.js
@@ -2,6 +2,10 @@ export default function chemicalFormula(formula, charge) {
   if (formula === null || formula === undefined) {
     return '';
   }
+  if (/-\w/g.test(formula)) {
+    // avoid bad formatting of metabolite names
+    return formula;
+  }
   let form = formula.replace(/([0-9])/g, '<sub>$1</sub>');
   if (charge) {
     form = `${form}<sup>${Math.abs(charge) !== 1 ? Math.abs(charge) : ''}${charge > 0 ? '+' : '-'}</sup>`;
