b91988824c26da0118d9dffc62f73d2db821e00d
e0 <shan.huang@me.com>
2021-11-16 11:00:05 +0100

    docs: add comments about how the main thread and the worker thread communicate

--- api/src/workers/3d-network.js
+++ api/src/workers/3d-network.js
@@ -6,6 +6,9 @@ import { Worker } from 'worker_threads'
 // requests and should ideally respond as quickly as possible.
 
 export default (workerData) => new Promise((resolve, reject) => {
+  // Create a Worker thread that waits for a message that inclues
+  // an object of the shape `{ nodes, links }`. Once received, it 
+  // generates the 3D network layout and replies to the main thread.
   const worker = new Worker(`
     const { parentPort } = require('worker_threads');
     const populateWithLayout = require('./src/utils/3d-network.js');
@@ -19,5 +22,8 @@ export default (workerData) => new Promise((resolve, reject) => {
       reject(new Error(`stopped with  ${code} exit code`));
     }
   })
+
+  // Once the worker is created and setup. Post a message to it
+  // from the main thread.
   worker.postMessage(workerData);
 })
