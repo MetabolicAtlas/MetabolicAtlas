5763dba7c47cfdcd496669bac5bd1f5b5ad4db7e
e0 <shan.huang@me.com>
2021-12-13 16:56:26 +0100

    chore: remove unnecessary eslint rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,38 +1,24 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: ['plugin:vue/essential', 'plugin:vue/recommended', '@vue/airbnb', 'prettier'],
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-        'vue/singleline-html-element-content-newline': 'off',
-        'vue/html-closing-bracket-newline': 'off',
-        'vue/multiline-html-element-content-newline': 'off',
-        'vue/html-self-closing': 'off',
-        'vue/no-v-html': 'off',
-        'vue/require-default-prop': 'off',
-        'vue/multi-word-component-names': 'off',
-        'vue/no-useless-template-attributes': 'off',
-        'vue/no-lone-template': 'off',
-        'vue/order-in-components': 'off',
-        'vue/v-slot-style': 'off',
-        'no-unused-vars': [
-            'error',
-            {
-                vars: 'all',
-                args: 'all',
-                ignoreRestSiblings: false,
-                varsIgnorePattern: '[iI]gnored', // do not report var containing 'ignored'
-            },
-        ],
-        'import/no-named-default': 'off',
-        'import/extensions': 'off',
-        'function-paren-newline': 'off',
-        'object-curly-newline': 'off',
-    },
-    parserOptions: {
-        parser: 'babel-eslint',
-    },
-}
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'plugin:vue/recommended', '@vue/airbnb', 'prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-useless-template-attributes': 'off',
+    'vue/no-lone-template': 'off',
+    'vue/order-in-components': 'off',
+    'vue/v-slot-style': 'off',
+    'import/no-named-default': 'off',
+    'import/extensions': 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
