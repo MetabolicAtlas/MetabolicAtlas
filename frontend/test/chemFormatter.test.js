d84a2324df3249dc33dd6e766235ff348fc9e10b
nanjiangshu <nanjiang.shu@gmail.com>
2021-12-16 14:10:30 +0100

    test: another test for chemical formula with charge

--- frontend/test/chemFormatter.test.js
+++ frontend/test/chemFormatter.test.js
@@ -3,4 +3,7 @@ import chemicalFormula from '../src/helpers/chemical-formatters';
 test('test chemicalreactions', () => {
   expect(chemicalFormula('C24H32O8', 0)).toBe('C<sub>2</sub><sub>4</sub>H<sub>3</sub><sub>2</sub>O<sub>8</sub>');
 });
+test('test chemicalreactions', () => {
+  expect(chemicalFormula('H2PO4-', 0)).toBe('H<sub>2</sub>PO<sub>4</sub><sup>-</sup>');
+});
 
