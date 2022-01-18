d26f47e346961a4f6c2e9cb8eb41acfb2f962f73
e0 <shan.huang@me.com>
2022-01-18 18:50:35 +0100

    feat: add tests for the cross reference (external DB) endpoint

---
+++ api/test/crossReference.test.js
@@ -0,0 +1,19 @@
+import fetch from "node-fetch";
+
+describe("cross reference", () => {
+  test("should return more than one component", async () => {
+    const res = await fetch(`${API_BASE}/external-db/HMR%202.0/HMR_1157`);
+
+    const { components } = await res.json();
+    expect(components.length).toBeGreaterThan(1);
+  });
+
+  test("should return a TSV file for a given data source", async () => {
+    const res = await fetch(`${API_BASE}/external-db/Recon3D/RE3335C`);
+
+    const {
+      externalDb: { url },
+    } = await res.json();
+    expect(url.length).toBeGreaterThan(1);
+  });
+});
