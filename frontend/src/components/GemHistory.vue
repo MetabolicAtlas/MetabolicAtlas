660291cfd5ff67f13bda84486e5d41470a9427fa
e0 <shan.huang@me.com>
2022-04-27 15:17:14 +0200

    feat: add interactivity for timeline chart

--- frontend/src/components/GemHistory.vue
+++ frontend/src/components/GemHistory.vue
@@ -1,18 +1,121 @@
 <template>
-  <svg />
+  <div id="timeline-wrapper">
+    <svg ref="svg" />
+    <div
+      v-if="selectedVersion"
+      id="timeline-popover"
+      ref="popover"
+      class="box"
+      :style="{ top: popoverOffset.top, left: popoverOffset.left }"
+    >
+      <div v-if="currentVersionData">
+        <p>
+          <router-link :to="{ name: 'browser', params: { model: selectedVersion.model } }">
+            <span class="icon pr-2"><i class="fa fa-table"></i></span>
+            {{ messages.gemBrowserName }}
+          </router-link>
+        </p>
+        <p>
+          <router-link :to="{ name: 'viewer', params: { model: selectedVersion.model } }">
+            <span class="icon pr-2"><i class="fa fa-map-o"></i></span>
+            {{ messages.mapViewerName }}
+          </router-link>
+        </p>
+      </div>
+      <a :href="selectedVersion.releaseLink" target="_blank">
+        <span class="icon pr-2"><i class="fa fa-github"></i></span>
+        Release notes
+      </a>
+      <div v-if="currentVersionData" class="pt-2 is-size-7">
+        <p>Reactions: {{ currentVersionData.reactionCount }}</p>
+        <p>Metabolites: {{ currentVersionData.metaboliteCount }}</p>
+        <p>Genes: {{ currentVersionData.geneCount }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
+import { mapGetters } from 'vuex';
+import { default as messages } from '@/content/messages';
 
 export default {
   data() {
-    return {};
+    return {
+      selectedVersion: null,
+      popoverOffset: {
+        top: 0,
+        left: 0,
+      },
+      messages,
+    };
   },
   mounted() {
     this.drawTimeline();
+    this.setupInteractivity();
+  },
+  computed: {
+    ...mapGetters({
+      integratedModels: 'models/integratedModels',
+    }),
+    currentVersionData() {
+      if (!this.selectedVersion) {
+        return null;
+      }
+
+      const { model, version } = this.selectedVersion;
+      const integratedModel = this.integratedModels.find(m => m.short_name === model);
+
+      if (integratedModel.version !== version) {
+        return null;
+      }
+
+      return {
+        reactionCount: integratedModel.reaction_count,
+        metaboliteCount: integratedModel.metabolite_count,
+        geneCount: integratedModel.gene_count,
+      };
+    },
   },
   methods: {
+    setupInteractivity() {
+      const circles = this.$refs.svg.getElementsByTagName('circle');
+
+      Array.from(circles).forEach(circle => {
+        circle.addEventListener('click', e => {
+          this.clearSelection();
+
+          const c = e.path[0];
+          c.classList.add('selected');
+          this.selectedVersion = {
+            ...c.dataset,
+            element: c,
+          };
+
+          // timeout is needed to make sure the popover element appears before calculation
+          setTimeout(() => {
+            this.popoverOffset = {
+              top: `${parseFloat(c.getAttribute('cy')) + 30}px`, // below version number
+              left: `${parseFloat(c.getAttribute('cx')) - this.$refs.popover.offsetWidth / 2}px`,
+            };
+          }, 0);
+        });
+      });
+
+      this.$refs.svg.addEventListener('click', e => {
+        const el = e.path[0];
+        if (el.tagName !== 'circle') {
+          this.clearSelection();
+        }
+      });
+    },
+    clearSelection() {
+      if (this.selectedVersion) {
+        this.selectedVersion.element.classList.remove('selected');
+        this.selectedVersion = null;
+      }
+    },
     drawTimeline() {
       const humanGem = [
         {
@@ -32,7 +135,7 @@ export default {
         {
           id: 'Human-GEM-1.2.0',
           parentId: [{ id: 'Human-GEM-1.1.0', citLink: '' }],
-          releaseDate: '2019-06-12',
+          releaseDate: '2019-07-12',
           releaseLink: 'https://github.com/SysBioChalmers/Human-GEM/releases/tag/v1.2.0',
           PMID: 32209698,
         },
@@ -65,10 +168,10 @@ export default {
           PMID: 32209698,
         },
         {
-          id: 'Human-GEM-1.7.0',
+          id: 'Human-GEM-1.10.0',
           parentId: [{ id: 'Human-GEM-1.6.0', citLink: '' }],
           releaseDate: '2021-09-15',
-          releaseLink: 'https://github.com/SysBioChalmers/Human-GEM/releases/tag/v1.7.0',
+          releaseLink: 'https://github.com/SysBioChalmers/Human-GEM/releases/tag/v1.10.0',
           PMID: 32209698,
         },
       ];
@@ -244,22 +347,31 @@ export default {
         .selectAll('circle')
         .data(humanGem.map(v => new Date(v.releaseDate)))
         .join('circle')
+        .classed('circle', true)
         .attr('r', (_d, i) => {
           const idParts = humanGem[i].id.split('.');
           const isMajor = idParts[1] === '0' && idParts[2] === '0';
-          return isMajor ? 10 : 5;
+          return isMajor ? 24 : 18;
         })
         .attr('cy', 50)
-        .attr('cx', d => timeScale(d));
+        .attr('cx', d => timeScale(d))
+        .attr('data-model', (_d, i) => humanGem[i].id.match(/\w+-\w+/)[0])
+        .attr('data-version', (_d, i) => humanGem[i].id.split('-')[2])
+        .attr('data-release-date', (_d, i) => humanGem[i].releaseDate)
+        .attr('data-release-link', (_d, i) => humanGem[i].releaseLink)
+        .attr('data-pmid', (_d, i) => humanGem[i].PMID);
 
       svg
         .append('g')
         .selectAll('text')
         .data(humanGem.map(v => new Date(v.releaseDate)))
         .join('text')
+        .classed('label', true)
+        .style('fill', 'white')
         .style('font-size', '10px')
-        .attr('y', 70)
-        .attr('x', d => timeScale(d))
+        .style('font-weight', 'bold')
+        .attr('y', 53)
+        .attr('x', (d, i) => timeScale(d) - (humanGem[i].id.split('.')[1] > 9 ? 5 : 2))
         .attr('transform', 'translate(-10, 0)')
         .text((_d, i) => humanGem[i].id.split('-')[2]);
 
@@ -284,10 +396,11 @@ export default {
         .selectAll('circle')
         .data(mouseGem.map(v => new Date(v.releaseDate)))
         .join('circle')
+        .classed('circle', true)
         .attr('r', (_d, i) => {
           const idParts = mouseGem[i].id.split('.');
           const isMajor = idParts[1] === '0' && idParts[2] === '0';
-          return isMajor ? 10 : 5;
+          return isMajor ? 22 : 18;
         })
         .attr('cy', 100)
         .attr('cx', d => timeScale(d));
@@ -297,9 +410,12 @@ export default {
         .selectAll('text')
         .data(mouseGem.map(v => new Date(v.releaseDate)))
         .join('text')
+        .classed('label', true)
+        .style('fill', 'white')
         .style('font-size', '10px')
-        .attr('y', 120)
-        .attr('x', d => timeScale(d))
+        .style('font-weight', 'bold')
+        .attr('y', 103)
+        .attr('x', d => timeScale(d) - 2)
         .attr('transform', 'translate(-10, 0)')
         .text((_d, i) => mouseGem[i].id.split('-')[2]);
 
@@ -324,10 +440,11 @@ export default {
         .selectAll('circle')
         .data(ratGem.map(v => new Date(v.releaseDate)))
         .join('circle')
+        .classed('circle', true)
         .attr('r', (_d, i) => {
           const idParts = ratGem[i].id.split('.');
           const isMajor = idParts[1] === '0' && idParts[2] === '0';
-          return isMajor ? 10 : 5;
+          return isMajor ? 24 : 18;
         })
         .attr('cy', 150)
         .attr('cx', d => timeScale(d));
@@ -337,9 +454,12 @@ export default {
         .selectAll('text')
         .data(ratGem.map(v => new Date(v.releaseDate)))
         .join('text')
+        .classed('label', true)
+        .style('fill', 'white')
         .style('font-size', '10px')
-        .attr('y', 170)
-        .attr('x', d => timeScale(d))
+        .style('font-weight', 'bold')
+        .attr('y', 153)
+        .attr('x', d => timeScale(d) - 2)
         .attr('transform', 'translate(-10, 0)')
         .text((_d, i) => ratGem[i].id.split('-')[2]);
 
@@ -364,10 +484,11 @@ export default {
         .selectAll('circle')
         .data(yeastGem.map(v => new Date(v.releaseDate)))
         .join('circle')
+        .classed('circle', true)
         .attr('r', (_d, i) => {
           const idParts = yeastGem[i].id.split('.');
           const isMajor = idParts[1] === '0' && idParts[2] === '0';
-          return isMajor ? 10 : 5;
+          return isMajor ? 24 : 18;
         })
         .attr('cy', 200)
         .attr('cx', d => timeScale(d));
@@ -377,12 +498,54 @@ export default {
         .selectAll('text')
         .data(yeastGem.map(v => new Date(v.releaseDate)))
         .join('text')
+        .classed('label', true)
+        .style('fill', 'white')
         .style('font-size', '10px')
-        .attr('y', 220)
-        .attr('x', d => timeScale(d))
+        .style('font-weight', 'bold')
+        .attr('y', 203)
+        .attr('x', d => timeScale(d) - 2)
         .attr('transform', 'translate(-10, 0)')
         .text((_d, i) => yeastGem[i].id.split('-')[2]);
     },
   },
 };
 </script>
+
+<style lang="scss">
+#timeline-wrapper {
+  position: relative;
+
+  svg {
+    .circle {
+      cursor: pointer;
+
+      &:hover,
+      &.selected {
+        fill: $primary-light;
+      }
+    }
+
+    .label {
+      pointer-events: none;
+    }
+  }
+
+  #timeline-popover {
+    position: absolute;
+    min-width: 150px;
+    background-color: hsl(0, 0%, 97%);
+    box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%), 0 0px 0 0 rgb(10 10 10 / 2%);
+
+    &:before {
+      position: absolute;
+      z-index: 1;
+      content: '';
+      right: calc(50% - 12px);
+      top: -10px;
+      border-style: solid;
+      border-width: 0 12px 12px 12px;
+      border-color: transparent transparent hsl(0, 0%, 97%) transparent;
+    }
+  }
+}
+</style>
