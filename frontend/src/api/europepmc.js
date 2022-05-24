66c113449ea3039c7d46a4f503888b20d1e421d2
e0 <shan.huang@me.com>
2022-05-24 13:29:20 +0200

    refactor: move Europe PMC formatting logic to store module and use optional chaining instead of forEach and try + catch

--- frontend/src/api/europepmc.js
+++ frontend/src/api/europepmc.js
@@ -5,39 +5,7 @@ const searchReferences = async queryIds => {
     `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${queryIds}&resultType=core&format=json`
   );
 
-  const refs = {};
-
-  data.resultList.result.forEach(details => {
-    try {
-      const refDetails = {};
-      if (!details.fullTextUrlList) {
-        refDetails.link = null;
-      } else {
-        refDetails.link = details.fullTextUrlList.fullTextUrl.filter(
-          e => e.documentStyle === 'html' && e.site === 'Europe_PMC'
-        );
-        if (refDetails.link.length === 0) {
-          refDetails.link = details.fullTextUrlList.fullTextUrl.filter(
-            e => e.documentStyle === 'doi' || e.documentStyle === 'abs'
-          )[0].url;
-        } else {
-          refDetails.link = refDetails.link[0].url;
-        }
-      }
-      if (details.pubYear) {
-        refDetails.year = details.pubYear;
-      }
-      refDetails.authors = details.authorList.author.map(e => e.fullName);
-      refDetails.journal = details.journalInfo.journal.title;
-      refDetails.title = details.title;
-      refDetails.formattedString = `${refDetails.authors.join(', ')}, ${refDetails.year}. <i>${
-        refDetails.title
-      }</i> ${refDetails.journal}`;
-      refs[details.id] = refDetails;
-    } catch {} // eslint-disable-line no-empty
-  });
-
-  return refs;
+  return data.resultList.result;
 };
 
 export default { searchReferences };
