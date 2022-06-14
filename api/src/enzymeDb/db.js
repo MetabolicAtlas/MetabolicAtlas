0fecd7cd75c5c61e7da91bbe7db229a1f7de6dff
e0 <shan.huang@me.com>
2022-06-14 11:39:11 +0200

    fix: add custom parser for the numeric type

--- api/src/enzymeDb/db.js
+++ api/src/enzymeDb/db.js
@@ -1,7 +1,14 @@
 import postgres from 'postgres';
 
+const configOptions = {
+  types: {
+    numeric: { to: 0, from: [1700], serialize: x => '' + x, parse: x => +x },
+  },
+};
+
 const sql = postgres(
-  `postgres://postgres:${process.env.POSTGRES_PASSWORD}@pg:5432/postgres`
+  `postgres://postgres:${process.env.POSTGRES_PASSWORD}@pg:5432/postgres`,
+  configOptions
 );
 
 export default sql;
