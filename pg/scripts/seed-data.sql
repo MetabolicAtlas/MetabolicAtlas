ba8cd7d0d9c946dd53f82533b42faee98c01262a
e0 <shan.huang@me.com>
2022-06-21 15:32:01 +0200

    chore: remove obsolete scripts

--- pg/scripts/seed-data.sql
+++
@@ -1,7 +0,0 @@
--- ma-exec pg psql -f scripts/seed-data.sql -U postgres
-
-copy enzymes(protein, organism, domain, ko, reaction_id, ec_number, compound, kcat_values) from program 'cat /input_data/enzymes/*.txt' delimiter E'\t';
-
-copy compounds from '/input_data/supplementary/compound.txt' delimiter E'\t' CSV HEADER;
-copy ec from '/input_data/supplementary/ec.txt' quote E'\u0007' delimiter E'\t' CSV HEADER;
-copy reactions from '/input_data/supplementary/reaction.txt' delimiter E'\t' CSV HEADER;
