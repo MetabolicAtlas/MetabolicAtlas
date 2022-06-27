3e24dd7cca8d34b13e532fa28c7168c2e9e9df18
e0 <shan.huang@me.com>
2022-06-27 14:02:28 +0200

    feat: do not index proteins for fuzzy search

--- pg/scripts/init.sql
+++ pg/scripts/init.sql
@@ -73,13 +73,13 @@ create index on enzymes using gin (string_to_array(ec_number, ';'));
 
 
 
--- create search indexes along with materialized views
+-- create search indexes along with materialized views (proteins disabled for now for performance reasons)
 create index on compounds using gist(name);
 create index on ec using gist(name);
 create index on reactions using gist(name);
 
-create materialized view enzymes_proteins as select protein from enzymes group by protein;
-create index on enzymes_proteins using gist(protein);
+-- create materialized view enzymes_proteins as select protein from enzymes group by protein;
+-- create index on enzymes_proteins using gist(protein);
 create materialized view enzymes_organisms as select organism from enzymes group by organism;
 create index on enzymes_organisms using gist(organism);
 create materialized view enzymes_domains as select domain from enzymes group by domain;
@@ -100,8 +100,6 @@ union all
 select text 'compounds' as origin_table, kegg as id, name as search_field from compounds
 union all
 select text 'ec' as origin_table, ec as id, name as search_field from ec
-union all
-select text 'enzymes_proteins' as origin_table, protein as id, protein as search_field from enzymes_proteins
 union all
 select text 'enzymes_organisms' as origin_table, organism as id, organism as search_field from enzymes_organisms
 union all
