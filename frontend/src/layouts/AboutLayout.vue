93f8f2ce2b36d83570d44ffde6b68ea066d8faa9
Mihail Anton <mihail.anton@chalmers.se>
2022-07-28 13:45:23 +0300

    fix: only add title if there is one

--- frontend/src/layouts/AboutLayout.vue
+++ frontend/src/layouts/AboutLayout.vue
@@ -4,7 +4,7 @@
       <div class="columns is-variable is-8 pt-5">
         <TableOfContents :links="tocLinks" />
         <div class="column content">
-          <h3 class="title is-3 mt-0">{{ title }}</h3>
+          <h3 v-if="title" class="title is-3 mt-0">{{ title }}</h3>
           <div :class="{ 'content-container': !fullContentWidth }">
             <slot name="contents" />
           </div>
