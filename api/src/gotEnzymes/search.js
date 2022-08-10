5910cd15745fbe584963581387b3f8f970d1a61c
MalinAhlberg <malin.ahlberg@nbis.se>
2022-08-10 07:37:11 +0200

    feat: replace '.' with standard char for ec searches

--- api/src/gotEnzymes/search.js
+++ api/src/gotEnzymes/search.js
@@ -3,7 +3,14 @@ import sql from 'gotEnzymes/db';
 const search = async searchTerm => {
   const term = searchTerm.toLowerCase();
   const fuzzyQuery = sql`
-    select type, id, match, ${sql`similarity(match, ${term})`} as score
+    select
+      type,
+      id,
+      match,
+      ${sql`similarity(
+        ${sql`replace(match, '.', 'a')`},
+        ${sql`replace(${term}, '.', 'a')`}
+      )`} as score
     from multi_search
     where match % ${term}
     order by score desc
@@ -16,20 +23,13 @@ const search = async searchTerm => {
     where kegg = ${searchTerm}
   `;
 
-  const ecMatchQuery = sql`
-    select 'ec' as type, ec as id, ec as match, ${sql`similarity(ec, ${searchTerm})`} as score
-    from ec
-    where ec LIKE ${searchTerm} || '%'
-  `;
-
   await sql`set pg_trgm.similarity_threshold = 0.25`; // this allows more matches, default is 0.3
-  const [fuzzyResults, geneResults, ecResults] = await Promise.all([
+  const [fuzzyResults, geneResults] = await Promise.all([
     fuzzyQuery,
     geneMatchQuery,
-    ecMatchQuery,
   ]);
 
-  return [...geneResults, ...ecResults, ...fuzzyResults].slice(0, 10);
+  return [...geneResults, ...fuzzyResults].slice(0, 10);
 };
 
 export default search;
