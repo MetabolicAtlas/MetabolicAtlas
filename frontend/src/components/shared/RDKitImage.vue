7ba6e0afb02e00d6d1ccdb2148502e2c4754938a
e0 <shan.huang@me.com>
2022-08-22 12:58:04 +0200

    feat: add RDKit image for GotEnzymes compounds

---
+++ frontend/src/components/shared/RDKitImage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div ref="imgWrapper">
+    <loader v-if="loading" />
+  </div>
+</template>
+
+<script>
+import Loader from '@/components/Loader.vue';
+
+export default {
+  name: 'RDKitImage',
+  components: {
+    Loader,
+  },
+  props: {
+    smiles: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  async mounted() {
+    try {
+      const RDKit = await window.initRDKitModule();
+      const mol = RDKit.get_mol(this.smiles);
+      const svg = mol.get_svg(800, 800);
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = svg;
+      tempDiv.querySelector('svg').setAttribute('width', '100%');
+      tempDiv.querySelector('svg').setAttribute('height', '100%');
+      this.$refs.imgWrapper.innerHTML = tempDiv.innerHTML;
+      tempDiv.remove();
+    } finally {
+      this.loading = false;
+    }
+  },
+};
+</script>
