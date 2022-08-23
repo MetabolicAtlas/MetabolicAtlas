ea61ec1fc9266e38635a7b393779f47bef0eb4ec
e0 <shan.huang@me.com>
2022-08-23 10:55:55 +0200

    feat: add functionality to see larger image in modal

--- frontend/src/components/shared/RDKitImage.vue
+++ frontend/src/components/shared/RDKitImage.vue
@@ -1,16 +1,29 @@
 <template>
-  <div id="rdkit-img-wrapper" ref="imgWrapper">
-    <loader v-if="loading" />
+  <div>
+    <div
+      id="rdkit-img-wrapper"
+      class="is-clickable"
+      title="Click to see larger image"
+      @click="handleClick"
+    >
+      <loader v-if="loading" />
+    </div>
+    <Modal :show-modal.sync="showLargeImage">
+      <div id="rdkit-modal-img-wrapper" />
+      <loader v-if="loading" />
+    </Modal>
   </div>
 </template>
 
 <script>
 import Loader from '@/components/Loader.vue';
+import Modal from '@/components/shared/Modal.vue';
 
 export default {
   name: 'RDKitImage',
   components: {
     Loader,
+    Modal,
   },
   props: {
     smiles: {
@@ -21,22 +34,47 @@ export default {
   data() {
     return {
       loading: true,
+      config: {
+        width: 800,
+        height: 800,
+        addAtomIndices: true,
+        addBondIndices: true,
+        addStereoAnnotation: true,
+        explicitMethyl: true,
+      },
+      mol: null,
+      showLargeImage: false,
     };
   },
   async mounted() {
-    try {
-      const RDKit = await window.initRDKitModule();
-      const mol = RDKit.get_mol(this.smiles);
-      const svg = mol.get_svg(800, 800);
-      const tempDiv = document.createElement('div');
-      tempDiv.innerHTML = svg;
-      tempDiv.querySelector('svg').setAttribute('width', '100%');
-      tempDiv.querySelector('svg').setAttribute('height', '100%');
-      this.$refs.imgWrapper.innerHTML = tempDiv.innerHTML;
-      tempDiv.remove();
-    } finally {
-      this.loading = false;
-    }
+    const RDKit = await window.initRDKitModule();
+    this.mol = RDKit.get_mol(this.smiles);
+    this.renderMolecule({ id: 'rdkit-img-wrapper' });
+  },
+  methods: {
+    handleClick() {
+      this.showLargeImage = true;
+
+      setTimeout(() => this.renderMolecule({ id: 'rdkit-modal-img-wrapper', isLarge: true }), 0);
+    },
+    renderMolecule({ id, isLarge = false }) {
+      try {
+        const config = {
+          ...this.config,
+          width: this.config.width * (isLarge ? 2 : 1),
+          height: this.config.height * (isLarge ? 2 : 1),
+        };
+        const svg = this.mol.get_svg_with_highlights(JSON.stringify(config));
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = svg;
+        tempDiv.querySelector('svg').setAttribute('width', '100%');
+        tempDiv.querySelector('svg').setAttribute('height', '100%');
+        document.getElementById(id).innerHTML = tempDiv.innerHTML;
+        tempDiv.remove();
+      } finally {
+        this.loading = false;
+      }
+    },
   },
 };
 </script>
