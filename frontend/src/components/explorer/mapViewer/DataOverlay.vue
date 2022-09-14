24879a4d308eed21b8c323956e613e245144a1b5
e0 <shan.huang@me.com>
2022-09-14 13:15:48 +0200

    fix: vue 3 warnings for data overlay

--- frontend/src/components/explorer/mapViewer/DataOverlay.vue
+++ frontend/src/components/explorer/mapViewer/DataOverlay.vue
@@ -67,7 +67,7 @@
         ></div>
       </div>
       <div v-else class="mt-2 is-flex is-justify-content-center">
-        <button class="button is-primary" @click="addSourceToIndex">Upload</button>
+        <button type="button" class="button is-primary" @click="addSourceToIndex">Upload</button>
       </div>
     </Modal>
     <div v-for="(chosenType, index) in filteredDataTypes" :key="index">
@@ -118,7 +118,7 @@
               <div v-if="dataSources.length > index" class="control">
                 <p>
                   Values from
-                  <a :href="dataSources[index].link" target="_blank">{{
+                  <a :href="dataSources[index].link" target="_blank" rel="noopener noreferrer">{{
                     dataSources[index].name
                   }}</a>
                 </p>
@@ -150,7 +150,9 @@
       :title="addCards() ? '' : 'No more data types available for this model'"
     >
       <span class="has-nowrap">
-        <button class="button" :disabled="!addCards()" @click="addOverlayCard">Add overlay</button>
+        <button type="button" class="button" :disabled="!addCards()" @click="addOverlayCard">
+          Add overlay
+        </button>
       </span>
     </div>
     <RNALegend class="my-3" />
