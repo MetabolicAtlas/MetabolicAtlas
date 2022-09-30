b6540ffa7206dd56f7d760038010e08d7b1b3879
Mihail Anton <mihail.anton@chalmers.se>
2022-09-30 20:36:29 +0200

    feat: update 3.0 citation

---
+++ frontend/src/components/about/Citation.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <h4 :id="entry.id" class="title is-5 pt-6">{{ entry.header }}</h4>
+    <p v-html="entry.text"></p>
+    <div class="columns is-mobile">
+      <div class="column is-2">
+        <a :href="entry.journalLink" target="_blank" rel="noopener noreferrer">
+          <img :src="entry.img" />
+        </a>
+      </div>
+      <div class="column">
+        <p>{{ entry.authors }}</p>
+        <p>
+          <b>{{ entry.title }}</b>
+          <span class="is-block"
+            ><i>{{ entry.journal }}</i></span
+          >
+        </p>
+        <p>
+          PubMed:
+          <a :href="pmidref()" target="_blank" rel="noopener noreferrer">{{ entry.pmid }}</a>
+          DOI:
+          <a :href="doiref()" target="_blank" rel="noopener noreferrer">
+            {{ entry.doi }}
+          </a>
+        </p>
+        <div class="is-flex is-justify-content-flex-start">
+          <a
+            :href="plumxref()"
+            data-popup="right"
+            data-size="medium"
+            class="plumx-plum-print-popup"
+            data-site="plum"
+            data-hide-when-empty="true"
+          ></a>
+          <div
+            data-badge-popover="right"
+            data-badge-type="donut"
+            :data-doi="entry.doi"
+            data-hide-no-mentions="true"
+            class="altmetric-embed px-2"
+          ></div>
+          <div
+            class="__dimensions_badge_embed__ px-2"
+            :data-doi="entry.doi"
+            data-style="small_circle"
+          ></div>
+          <div
+            class="scite-badge px-2"
+            :data-doi="entry.doi"
+            data-layout="vertical"
+            data-show-zero="false"
+            data-small="true"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Citation',
+  props: {
+    entry: {
+      type: Object,
+    },
+  },
+  methods: {
+    plumxref() {
+      return `https://plu.mx/plum/a/?doi=${encodeURI(this.entry.doi)}`;
+    },
+    doiref() {
+      return `https://doi.org/${encodeURI(this.entry.doi)}`;
+    },
+    pmidref() {
+      return `https://pubmed.ncbi.nlm.nih.gov/${encodeURI(this.entry.pmid)}`;
+    },
+  },
+};
+</script>
+
+<style></style>
