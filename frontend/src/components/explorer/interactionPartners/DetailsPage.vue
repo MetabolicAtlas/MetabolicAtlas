495635a7bfd5742d6712a3daaaba06675f4df48f
MalinAhlberg <malin.ahlberg@nbis.se>
2022-10-19 13:13:07 +0200

    refactor: reoder arguments to fix eslint warning

--- frontend/src/components/explorer/interactionPartners/DetailsPage.vue
+++ frontend/src/components/explorer/interactionPartners/DetailsPage.vue
@@ -25,7 +25,7 @@
               ></h3>
             </div>
           </div>
-          <context-menu :show="showGraphContextMenu" :expand="loadExpansion" ref="contextMenu" />
+          <context-menu :ref="contextMenu" show="showGraphContextMenu" :expand="loadExpansion" />
           <div class="container is-fullhd columns is-multiline">
             <div class="column is-8-desktop is-fullwidth-tablet">
               <div id="viewer-container">
