7b79e0cc04d728de53d9698d23d865d33520e3e9
Nanjiang Shu <nanjiang.shu@gmail.com>
2022-10-20 10:12:46 +0200

    fix: network map in front of quick search area

--- frontend/src/components/explorer/interactionPartners/DetailsPage.vue
+++ frontend/src/components/explorer/interactionPartners/DetailsPage.vue
@@ -26,7 +26,7 @@
             </div>
           </div>
           <context-menu ref="contextMenu" :show="showGraphContextMenu" :expand="loadExpansion" />
-          <div class="container is-fullhd columns is-multiline">
+          <div id="mapWrapper" class="container is-fullhd columns is-multiline">
             <div class="column is-8-desktop is-fullwidth-tablet">
               <div id="viewer-container">
                 <div id="dropdownMenuExport" class="dropdown">
@@ -743,6 +743,10 @@ export default {
     width: 100%;
     height: 100%;
     position: relative;
+  }
+
+  #mapWrapper {
+    z-index: 5;
   }
 
   #viewer3d {
