ef5e098c0e2920f4ee6afbbe6c9edc34a306259e
Nanjiang Shu <nanjiang.shu@gmail.com>
2022-11-18 22:41:44 +0100

    fix: eslint problem

--- frontend/src/helpers/chemical-formatters.js
+++ frontend/src/helpers/chemical-formatters.js
@@ -18,17 +18,15 @@ const chemicalFormula = (formula, charge) => {
     form = `${form}<sup>${Math.abs(charge) !== 1 ? Math.abs(charge) : ''}${
       charge > 0 ? '+' : '-'
     }</sup>`;
+  } else if (/\b(Ca|Fe|Cu|Mg)\d\+/.test(formula)) {
+    // for cations with multiple charges, format both the charge number
+    // and charge sign as superscript
+    form = formula.replace(/([0-9]\+)/g, '<sup>$1</sup>');
   } else {
     // if no explicit charge is provided, check if the form includes one
     // useful for showing eg NADP+ rather than the formal charge NADP3-
-    if (/\b(Ca|Fe|Cu|Mg)\d\+/.test(formula)) {
-      // for cations with multiple charges, format both the charge number
-      // and charge sign as superscript
-      form = formula.replace(/([0-9]\+)/g, '<sup>$1</sup>');
-    } else {
-      form = formula.replace(/([0-9])/g, '<sub>$1</sub>');
-      form = form.replace(/([-+])$/, '<sup>$1</sup>');
-    }
+    form = formula.replace(/([0-9])/g, '<sub>$1</sub>');
+    form = form.replace(/([-+])$/, '<sup>$1</sup>');
   }
   return form;
 };
