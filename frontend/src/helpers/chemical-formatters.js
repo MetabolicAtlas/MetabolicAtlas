fd9ac8540148b881efe91503572e5d660b32dd68
Nanjiang Shu <nanjiang.shu@gmail.com>
2022-11-18 22:14:54 +0100

    fix: bad formatting for cations with multiple charges

--- frontend/src/helpers/chemical-formatters.js
+++ frontend/src/helpers/chemical-formatters.js
@@ -12,15 +12,23 @@ const chemicalFormula = (formula, charge) => {
     // avoid bad formatting of metabolite names
     return formula;
   }
-  let form = formula.replace(/([0-9])/g, '<sub>$1</sub>');
+  let form;
   if (charge) {
+    form = formula.replace(/([0-9])/g, '<sub>$1</sub>');
     form = `${form}<sup>${Math.abs(charge) !== 1 ? Math.abs(charge) : ''}${
       charge > 0 ? '+' : '-'
     }</sup>`;
   } else {
     // if no explicit charge is provided, check if the form includes one
     // useful for showing eg NADP+ rather than the formal charge NADP3-
-    form = form.replace(/([-+])$/, '<sup>$1</sup>');
+    if (/\b(Ca|Fe|Cu|Mg)\d\+/.test(formula)) {
+      // for cations with multiple charges, format both the charge number
+      // and charge sign as superscript
+      form = formula.replace(/([0-9]\+)/g, '<sup>$1</sup>');
+    } else {
+      form = formula.replace(/([0-9])/g, '<sub>$1</sub>');
+      form = form.replace(/([-+])$/, '<sup>$1</sup>');
+    }
   }
   return form;
 };
