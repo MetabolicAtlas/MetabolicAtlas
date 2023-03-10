4473183f93d813761c546902d8af4e5fd3bf21a8
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2023-03-10 14:31:51 +0100

    fix: Quote unquoted variables, use descriptive options

--- neo4j/import.sh
+++ neo4j/import.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Set the password
-neo4j-admin set-initial-password $NEO4J_PASSWORD
+neo4j-admin set-initial-password "$NEO4J_PASSWORD"
 
 if [ ! -e "/var/lib/neo4j/logs/neo4j.log" ] ; then
   touch /var/lib/neo4j/logs/neo4j.log
@@ -13,14 +13,18 @@ fi
 neo4j start
 
 # Wait for server to start
-while [ -z "$(grep 'Started.' /var/lib/neo4j/logs/neo4j.log)" ]
+until grep -qF 'Started.' /var/lib/neo4j/logs/neo4j.log
 do
   echo Waiting for neo4j to start
   sleep 1
 done
 
 # Import data
-cypher-shell -u $NEO4J_USERNAME -p $NEO4J_PASSWORD --format plain --file import/import.cypher
+cypher-shell \
+  --username "$NEO4J_USERNAME" \
+  --password "$NEO4J_PASSWORD" \
+  --format plain \
+  --file import/import.cypher
 
 # Stop server
 neo4j stop
