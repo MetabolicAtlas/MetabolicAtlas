5f9e3b9f60d703b35d784164f66aadd3ef7a8ba1
Per Johnsson <per.johnsson@nbis.se>
2023-04-04 10:45:15 +0200

    Fix: Text/form reordering

--- frontend/src/components/about/Platform.vue
+++ frontend/src/components/about/Platform.vue
@@ -245,19 +245,18 @@
       </p>
 
       <h3 id="contact-us" class="title is-3 mt-6">Contact us</h3>
+      <p>
+        For any questions, comments or suggestions, or if you find any bugs or vulnerabilities
+        regarding Metabolic Atlas, please email us at
+        <a href="mailto:contact@metabolicatlas.org" target="_blank" rel="noopener noreferrer"
+        >contact [at] metabolicatlas [dot] org</a>, or use the contact form below.
+      </p>
 
       <iframe
         title="Contact Form"
         src="https://docs.google.com/forms/d/e/1FAIpQLScnVFKd5OK9zIM9jR4qnnOr0QsZKZnhFql6saZuJaQYYsMs5g/viewform?embedded=true"
         width="640" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading…
       </iframe>
-      <p>
-        For any questions, comments or suggestions, or if you find any bugs or vulnerabilities
-        regarding Metabolic Atlas, please email us at
-        <a href="mailto:contact@metabolicatlas.org" target="_blank" rel="noopener noreferrer"
-          >contact [at] metabolicatlas [dot] org</a
-        >.
-      </p>
       <p>
         For any management related issues, such as collaborations or funding, please email us at
         <a href="mailto:management@metabolicatlas.org" target="_blank" rel="noopener noreferrer"
