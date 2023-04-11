d141d553c8fd6073893283ef34f04fac95c15559
jonandernovella <novella.uu@gmail.com>
2023-04-11 14:45:20 +0200

    fix: Improve accessibility of contact component
    - Added icons
    - Add link to email body
    - Styling fixes

--- frontend/src/components/shared/GemContact.vue
+++ frontend/src/components/shared/GemContact.vue
@@ -11,12 +11,16 @@
       <div v-if="isExpanded" class="message-body has-text-left content">
         Get in touch with the authors of {{ model.short_name }} to tell them what is wrong with this
         {{ type }}
-        <div class="contact">
-        <a :href="`mailto:${model.email}?subject=Issue on ${type} ${id}`"><i class="fa fa-envelope"/></a>
-          <a v-if="model.chat_link" :href="model.chat_link" target="_blank" rel="noopener noreferrer">
-            <i class="fa-thin fa-messages"/> </a>
-        <a :href="model.chat_link" target="_blank" rel="noopener noreferrer">
-          <i class="fa fa-github"/> </a>
+        <div class="contact mt-3">
+          <a :href="createMailLink(model.email, type, id)">
+            <i class="fa fa-envelope-o fa-lg" />
+          </a>
+          <a  v-if="model.chat_link" :href="model.chat_link" target="_blank" rel="noopener noreferrer">
+            <i class="fa fa-comment-o fa-lg" />
+          </a>
+          <a :href="model.link" target="_blank" rel="noopener noreferrer">
+            <i class="fa fa-github fa-lg" />
+          </a>
         </div>
       </div>
     </article>
@@ -28,6 +32,15 @@ import { mapState } from 'vuex';
 
 export default {
   name: 'GemContact',
+  methods: {
+    getPageURL() {
+      return `${window.location?.href}`
+    },
+    createMailLink(email, type, id) {
+      const body = `I have spotted an issue on the following page: ${this.getPageURL()}`
+      return `mailto:${email}?subject=Issue on ${type} ${id}&body=${body}`
+    }
+  },
   props: {
     type: {
       type: String,
@@ -54,6 +67,6 @@ export default {
 <style lang="scss">
 .contact {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
 }
 </style>
