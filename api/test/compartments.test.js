11b2ff2ddc30d09c1ddc2ceb0909dab8011ba164
Per Johnsson <per.johnsson@nbis.se>
2023-04-21 11:16:33 +0200

    Test: Tests bad model input in compartment endpoint

--- api/test/compartments.test.js
+++ api/test/compartments.test.js
@@ -36,12 +36,46 @@ describe('compartments', () => {
       expect(res.status).toBe(404);
     });
 
-    test('should return 400 if model contains parenthesis', async () => {
-      const res = await fetch(
-        `${API_BASE}/compartments/golgi_apparatus?model=HumanGem)-%5B%5D-(x&full=true`
-      );
-      expect(res.status).toBe(400);
-    });
+    const left_parenthesis = '(';
+    const right_parenthesis = ')';
+    const single_quote = "'";
+    const double_quote = '"';
+    const backtick = '`';
+    const colon = ':';
+    const left_square_bracket = '[';
+    const right_square_bracket = ']';
+    const slash = '/';
+    const backslash = '\\';
+    const comma = ',';
+    const left_curly_brace = '{';
+    const right_curly_brace = '}';
+
+    const MALICIOUS_CHARACTERS = [
+      left_parenthesis,
+      right_parenthesis,
+      single_quote,
+      double_quote,
+      backtick,
+      colon,
+      left_square_bracket,
+      right_square_bracket,
+      slash,
+      backslash,
+      comma,
+      left_curly_brace,
+      right_curly_brace,
+    ];
+    test.each(MALICIOUS_CHARACTERS)(
+      'should return 400 if model contains parenthesis',
+      async character => {
+        const res = await fetch(
+          `${API_BASE}/compartments/golgi_apparatus?model=${character}&full=true`
+        );
+        expect(res.status).toBe(400);
+        const data = await res.text();
+        expect(data).toBe('Malformed model');
+      }
+    );
   });
 
   describe('get related reactions', () => {
