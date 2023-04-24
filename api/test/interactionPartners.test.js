d031e9a11fb90c20a8e6912503cd4c92f237f60c
Per Johnsson <per.johnsson@nbis.se>
2023-04-24 08:51:08 +0200

    Test: Cypher injection tests for interaction partners API

--- api/test/interactionPartners.test.js
+++ api/test/interactionPartners.test.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import { MALICIOUS_CHARACTERS } from '../src/malicious-characters';
 
 describe('interaction partners', () => {
   test('the interaction partners should include a list of reactions', async () => {
@@ -23,4 +24,39 @@ describe('interaction partners', () => {
     );
     expect(res.status).toBe(404);
   });
+
+  test.each(MALICIOUS_CHARACTERS)(
+    'should return 400 if model contains %p',
+    async character => {
+      const res = await fetch(
+        `${API_BASE}/interaction-partners/ENSG00000120697?model=${character}&full=true`
+      );
+      expect(res.status).toBe(400);
+      const data = await res.text();
+      expect(data).toBe('Malicious char detected');
+    }
+  );
+
+  test.each(MALICIOUS_CHARACTERS)(
+    'should return 400 if version contains %p',
+    async character => {
+      const res = await fetch(
+        `${API_BASE}/interaction-partners/ENSG00000120697?model=HumanGem&version=${character}`
+      );
+      expect(res.status).toBe(400);
+      const data = await res.text();
+      expect(data).toBe('Malicious char detected');
+    }
+  );
+
+  test.each(MALICIOUS_CHARACTERS)(
+    'should return 400 or 404 if id contains %p',
+    async character => {
+      const res = await fetch(
+        `${API_BASE}/interaction-partners/${character}?model=HumanGem&version=${HUMAN_GEM_VERSION}`
+      );
+      // Slash or back-slash in path param provoke 404 instead of 400
+      expect([400, 404].includes(res.status)).toBeTruthy();
+    }
+  );
 });
